5a807857ad695d930ff744be9c751d6fed30779e
illeiva2 <illeiva@outlook.com>
2023-09-06 12:19:52 -0300

    fin task 3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,103 +1,122 @@
-const docLocation = document.getElementById("index-container") ? "i" :
-                 document.getElementById("past-container") ? "p" :
-                 document.getElementById("upcoming-container") ? "u" :
-                 false;
+const container = document.getElementById("card-container");
+const inputText = document.getElementById('search');
+const containerChecksPlace = document.getElementById("check-place");
+const containerChecks = document.getElementById("check");
+const containerChecksCategory = document.getElementById("check-category");
+const detContainer = document.getElementById("container");
+let arrayOfEvents = [];
+let urlDetails = ""
 
-let containerI = document.getElementById("index-container");
-let containerP = document.getElementById("past-container");
-let containerU = document.getElementById("upcoming-container");
+if (document.title === "Home") {
+    arrayOfEvents = data.events;
+    urlDetails = "./assets/pages/details.html"
+} else if (document.title === "Past Events") {
+    urlDetails = "./details.html"
+    arrayOfEvents = data.events.filter(evento => evento.date < data.currentDate);
+} else if (document.title === "Upcoming Events") {
+    urlDetails = "./details.html"
+    arrayOfEvents = data.events.filter(evento => evento.date > data.currentDate);
+} else if (document.title === "Details") {
+    const queryString = location.search
+    const params = new URLSearchParams(queryString)
+    const id = params.get("id")
+    let eventt = data.events.find((even) => even._id == id)
+    createCardDetails(eventt)
+}
+createCheckboxes(arrayOfEvents, "Category");
+createCheckboxes(arrayOfEvents, "Place");
+createCards(arrayOfEvents, container)
 
-const pEvents = []
-const uEvents = []
+inputText.addEventListener("input", () => {
+    let filtro = filterByText(arrayOfEvents, inputText.value);
+    let filtro2 = filterByCategory(filtro);
+    createCards(filtro2, container);
+});
 
-for (even of data.events) {
-    if (even.date > data.currentDate) {
-        uEvents.push(even)
+containerChecks.addEventListener("change", () => {
+    let filtro = filterByCategory(arrayOfEvents);
+    let filtro2 = filterByText(filtro, inputText.value);
+    createCards(filtro2, container);
+});
+
+function createCards(arrayEvent, container) {
+    if (arrayEvent == 0) {
+        container.innerHTML = `<h2 class="display-1 fw-bolder text-white text-center bg-danger rounded-3">No results to display</h2>`
+        return
     }
-    else {
-        pEvents.push(even)
-    }
+    let html = ''
+    arrayEvent.forEach(eventt => html += createCard(eventt))
+    container.innerHTML = html
 }
 
-function createCard(min, max) {
-    if (docLocation === "i") {
-        containerI.innerHTML = "";
-        for (eventt of data.events) {
-            if (min === 0 && max === 0) {
-                createEventCard(eventt);
-            } else if (eventt.price > min && eventt.price <= max) {
-                createEventCard(eventt);
-            }
-        }
-    }
-    else if (docLocation === "p") {
-        containerP.innerHTML = "";
-        for (eventt of pEvents) {
-            if (min === 0 && max === 0) {
-                createEventCard(eventt);
-            } else if (eventt.price > min && eventt.price <= max) {
-                createEventCard(eventt);
-            }
-        }
-    }
-    else if (docLocation === "u") {
-        containerU.innerHTML = "";
-        for (eventt of uEvents) {
-            if (min === 0 && max === 0) {
-                createEventCard(eventt);
-            } else if (eventt.price > min && eventt.price <= max) {
-                createEventCard(eventt);
-            }
-        }
-    }
-}
-
-function createEventCard(eventt) {
-    const card = document.createElement("div");
-    card.classList = "row py-3";
-    card.style.width = "18rem";
-    card.innerHTML = `<div class="card h-100">
+function createCard(eventt) {
+    return `<div class="card h-100 row py-3" style="width: 18rem">
         <img src="${eventt.image}" class="card-img-top" alt="${eventt.name}">
         <div class="card-body d-flex flex-column justify-content-between">
             <h5 class="card-title fs-3">${eventt.name}</h5>
             <div class="d-flex justify-content-between">
                 <p>${eventt.price} US$</p>
-                <a href="#" class="btn btn-primary flex-end">Details</a>
+                <a href="${urlDetails}?id=${eventt._id}" class="btn btn-primary flex-end">Details</a>
             </div>
         </div>
-    </div>`;
-    docLocation === "i"? containerI.appendChild(card) : docLocation === "u"? containerU.appendChild(card) : containerP.appendChild(card);
+    </div>`
 }
 
-createCard(0, 0)
+function filterByCategory(array) {
+    let arrayCheck = Array.from(document.querySelectorAll("input[type='checkbox']"))
+        .filter(check => check.checked)
+        .map(checked => checked.value);
+    console.log(arrayCheck);
+    if (arrayCheck.length == 0) {
+        return array;
+    }
+    return array.filter(element => {
+        return arrayCheck.some(category => element.category.includes(category)) ||
+            arrayCheck.some(place => element.place.includes(place));
+    });
+}
 
-const low = document.getElementById('low');
-const mid = document.getElementById('mid');
-const high = document.getElementById('high');
+function filterByText(array, text) {
+    const lowercaseText = text.toLowerCase();
+    return array.filter(element => element.name.toLowerCase().includes(lowercaseText));
+}
 
-low.addEventListener('change', function () {
-    if (this.checked) {
-        createCard(0, 50);
+function createCheckboxes(array, category) {
+    let html = '';
+    if (category === 'Place') {
+        let place = [...new Set(array.map(elemento => elemento.place))]
+        place.forEach(place => (html += createCheckbox(place)));
+        containerChecksPlace.innerHTML = html;
+    } else if (category === 'Category') {
+        let place = [...new Set(array.map(elemento => elemento.category))]
+        place.forEach(place => (html += createCheckbox(place)));
+        containerChecksCategory.innerHTML = html;
     }
-    else {
-        createCard(0, 0)
-    }
-});
 
-mid.addEventListener('change', function () {
-    if (this.checked) {
-        createCard(50, 225);
-    }
-    else {
-        createCard(0, 0)
-    }
-});
+}
 
-high.addEventListener('change', function () {
-    if (this.checked) {
-        createCard(225, Infinity);
-    }
-    else {
-        createCard(0, 0)
-    }
-});
+function createCheckbox(data) {
+    return `<li class="d-flex justify-content-between px-2"><div class="form-check form-switch">
+      <input class="form-check-input" type="checkbox" value="${data}" role="switch" id="${data}">
+      <label class="form-check-label" for="${data}">${data}</label>
+    </div></li>`;
+}
+
+function createCardDetails(eventt) {
+    let cardDetail = document.createElement("div")
+    cardDetail.className = "card"
+    cardDetail.style.maxWidth = "80%"
+    cardDetail.style.minHeight = "20rem"
+    cardDetail.style.padding = "0.5rem"
+    cardDetail.innerHTML = `<div class="card-body">
+    <h5 class="card-title">${eventt.name}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">${eventt.description}</h6>
+    <p class="card-text">Date: ${eventt.date}</p>
+    <p class="card-text">Category: ${eventt.category}</p>
+    <p class="card-text">Price: ${eventt.price}</p>
+    <p class="card-text">Place: ${eventt.place}</p>
+    <p class="card-text">Capacity: ${eventt.capacity}</p>
+    <p class="card-text">Estimated: ${eventt.estimate}</p>
+  </div>`
+  detContainer.appendChild(cardDetail)
+}
